001785d18297b4e32d12e0cf6e2df461b9c683ca
Adora20-sketch <olukolunimota20@gmail.com>
2026-02-04 11:15:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById('loginForm');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const matric = document.getElementById('matric').value.trim();
+      if (matric) {
+        localStorage.setItem('matric', matric);
+        window.location.href = "index.html";
+      } else {
+        alert("Please enter your matric number!");
+      }
+    });
+  }
+});
